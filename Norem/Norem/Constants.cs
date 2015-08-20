262f240f17c87cae68f2f817ebf1ea83a7b23565
antong liu <discomethod@qq.com>
2015-08-19 19:40:07 -0700

    remove redundant constant string

--- Norem/Norem/Constants.cs
+++ Norem/Norem/Constants.cs
@@ -3,7 +3,6 @@
     class Constants
     {
         public string URLLogin { get { return "/security/login.do"; } } // url for the login display page
-        //public string URLLoginDo { get { return "/security/dologin.do?username={0}&password={1}&{2}={3}";  } } // url for submitting the login form
         public string URLLoginDo { get { return "/security/dologin.do"; } } // url for submitting the login form
         public string URLPoxnora { get { return "http://www.poxnora.com"; } }
     }
